637bcbe831c961d665187fa3be5622433d6f865c
RomaSV <sve4nikov.roma@yandex.ru>
2018-04-28 17:07:17 +0300

    add more colors & layouts updates

--- app/src/main/java/com/dekinci/lksbstu/MainActivity.java
+++ app/src/main/java/com/dekinci/lksbstu/MainActivity.java
@@ -82,17 +82,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_schedule) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_messages) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
