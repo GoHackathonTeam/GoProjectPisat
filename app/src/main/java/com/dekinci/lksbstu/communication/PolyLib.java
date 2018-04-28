5c0c510f9a9756a01d505a40a5068c9ce94dc221
Lexcorp3439 <lr.romanov1999@mail.ru>
2018-04-28 12:57:00 +0300

    Compile retrofit

---
+++ app/src/main/java/com/dekinci/lksbstu/communication/PolyLib.java
@@ -0,0 +1,37 @@
+package com.dekinci.lksbstu.communication;
+
+import com.dekinci.lksbstu.utils.FactCallback;
+import com.dekinci.lksbstu.utils.ResultCallback;
+
+public class PolyLib implements PolyApi{
+
+    @Override
+    public void getUserinfo(int user_id, ResultCallback resultCallback) {
+
+    }
+
+    @Override
+    public void login(String login, String password, ResultCallback resultCallback) {
+
+    }
+
+    @Override
+    public void getSchedule(int group_id, ResultCallback resultCallback) {
+
+    }
+
+    @Override
+    public void sendTask(int user_id, int group_id, String msg, FactCallback factCallback) {
+
+    }
+
+    @Override
+    public void sendDoc(FactCallback factCallback) {
+
+    }
+
+    @Override
+    public void getNews(ResultCallback resultCallback) {
+
+    }
+}
